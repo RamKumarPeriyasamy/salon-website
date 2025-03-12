01ec2a3a0ccfaeaafc76d7a0cd10bcc17f65bc34
ramkumar <ramkumar.hexr@gmail.com>
2025-03-12 13:18:12 +0530

    update 002

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8001, // Set the port to 3000 (or any other port you prefer)
+  },
 })
